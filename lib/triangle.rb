a13a76132f5734282992d943533f82affc729fe9
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 19:25:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,14 +13,14 @@ class Triangle
       raise TriangleError
     elsif s1 <= 0 || s2 <= 0 || s3 <= 0
       raise TriangleError
-    # elsif s1 + s2 < s3 || s2 + s3 < s1 || s1 < s3 > s2
-    #   raise TriangleError
-  elsif s1 == s2 && s2 == s3 && s1 == s3
+    elsif s1 == s2 && s2 == s3 && s1 == s3
       :equilateral
     elsif s1 == s2 || s2 == s3 || s1 == s3
       :isosceles
-    else s1 != s2 && s1 != s3 && s2 != s3
+    elsif s1 != s2 && s1 != s3 && s2 != s3
       :scalene
+    else s1 + s2 < s3 || s2 + s3 < s1 || s1 < s3 > s2
+    #   raise TriangleError
     end
   end
 
