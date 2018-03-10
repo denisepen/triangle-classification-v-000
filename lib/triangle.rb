7d7efabef9fd8dc5bfe73a7f25bcb1882e45be1e
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 19:13:02 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
   def kind
     if s1 == 0 || s2 == 0 || s3 == 0
       raise TriangleError
-    elsif s1 =< 0 || s2 =< 0 || s3 =< 0
+    elsif s1 <= 0 || s2 <= 0 || s3 <= 0
       raise TriangleError
     else
     if s1 == s2 && s2 == s3 && s1 == s3
