cfe4c121c3d04be1207c664ac16cc530141b4858
Denise Pendleton <dpendl1@hotmail.com>
2018-03-10 18:46:04 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,10 @@ class Triangle
     @s3 = s3
   end
 
-  def kind()
-    if 
+  def kind(s1, s2, s3)
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 != s2 && s1 != s3 && s2 != s3
+      :scalene
   end
 end
